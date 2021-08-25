0e02cd7a16407b6f4d53a6ff54f9a6f24352f058
J.Eduardo <eduardoczv@gmail.com>
2021-08-25 14:56:13 -0500

    documentación de API con swagger

--- api/index.js
+++ api/index.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const config = require('../config.js');
 const user = require('../components/user/network');
 
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 
 app.use(bodyParser.json());
 
+const swaggerDoc = require('./swagger.json')
+
 //ROUETER
 app.use('/api/user',user);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api,()=>{
     console.log("Api escuchando en el puerto", config.api.port);
